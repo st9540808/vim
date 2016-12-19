25d5f900739d00150aba5059e29e49767bfcb277
st9540808 <st9540808@gmail.com>
2016-12-19 19:47:49 +0800

    2016 12/19 ubuntu 16.04

---
+++ install.sh
@@ -0,0 +1,8 @@
+cp .bashrc ~/
+cp .vimrc ~/
+cp -r .vim/ ~/
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/st9540808/2016-Fall-Operating-Systems ~/Desktop
+git clone https://github.com/st9540808/Ten-Major-Construction-Projects ~/Desktop
+git clone https://github.com/st9540808/Data-Structures ~/Desktop
