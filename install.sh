af69f97c46b3ba6fa85eca469fbba99d0188a46a
st9540808 <st9540808@gmail.com>
2016-12-21 09:23:44 +0800

    install imwheel and preference

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 cp .bashrc ~/
 cp .vimrc ~/
 cp -r .vim/ ~/
-cp .imwheelrc ~/
 
 cd ~/Desktop
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -12,4 +11,6 @@ git clone https://github.com/st9540808/Data-Structures
 sudo apt-get install htop
 sudo apt-get install software-center
 sudo apt-get install imwheel
-inwheel
+cp .imwheelrc ~/
+
+echo "inwheel" >> ~/.profile 
