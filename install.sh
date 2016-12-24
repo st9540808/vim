128ac914e7121d44b07fc7a1efee11d81de44d1b
st9540808 <st9540808@gmail.com>
2016-12-25 01:39:08 +0800

    ubuntu 16.04 <16.12.25>  add astyle

--- install.sh
+++ install.sh
@@ -4,13 +4,18 @@ cp -r .vim/ ~/
 
 cd ~/Desktop
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
 git clone https://github.com/st9540808/2016-Fall-Operating-Systems
 git clone https://github.com/st9540808/Ten-Major-Construction-Projects
 git clone https://github.com/st9540808/Data-Structures
+
 
 sudo apt-get install htop
 sudo apt-get install software-center
 sudo apt-get install imwheel
 cp .imwheelrc ~/
 
-echo "inwheel" >> ~/.profile 
+sudo apt-get install astyle
+cp .astylerc ~/
+
